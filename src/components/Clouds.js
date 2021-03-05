6ef39c75ff5a45cadd6dec9705503302b3484f1d
Evelyn Fredin <evefre2001@skola.goteborg.se>
2021-03-05 16:31:35 +0100

    SVG Animation

--- src/components/Clouds.js
+++ src/components/Clouds.js
@@ -1,12 +1,20 @@
-import Stripe from '../assets/images/stripe.svg'
+import { motion, useViewportScroll, useTransform } from "framer-motion"
+import Stripe from '../assets/images/strip.svg'
 
 const Clouds = () => {
+    const { scrollYProgress } = useViewportScroll();
+    const yPosAnim = useTransform(scrollYProgress, [0, 0.4, 1], [0, -550, -100]);
+    const xPosAnim = useTransform(scrollYProgress, [0, 0.4, 1], [-200, 500, -200]);
+
     return (
-        <section className="flex flex-col items-center h-auto mt-40 mb-32">
-            <img src={Stripe} alt="" className="w-3/4 md:w-auto"/>
+        <div className="flex flex-col items-center">
+            <motion.img src={Stripe} alt="" className="w-3/4 md:w-auto" style={{
+            x: yPosAnim}}/>
+
             <h2 className="uppercase text-3xl md:text-7xl my-5">Clouds (The Mixtape)</h2>
-            <img src={Stripe} alt="" className="w-3/4 md:w-auto"/>
-        </section>
+            <motion.img src={Stripe} alt="" className="w-3/4 md:w-auto" style={{
+            x: xPosAnim}}/>
+        </div>
     )
 }
 
